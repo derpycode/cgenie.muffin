a541b1f35defbb30def515fca8b4aa190f078665
derpycode <derp@derpy.co.uk>
2018-10-30 11:47:44 -0700

    altered tracer relationships for pyrite and siderite

--- genie-main/src/fortran/cmngem/gem_util.f90
+++ genie-main/src/fortran/cmngem/gem_util.f90
@@ -158,9 +158,12 @@ CONTAINS
     conv_sed_ocn(io_Fe,is_FeCO3)                = 1.0
     conv_sed_ocn(io_DIC_13C,is_FeCO3_13C)       = 1.0
     conv_sed_ocn(io_Fe_56Fe,is_FeCO3_56Fe)      = 1.0
-    conv_sed_ocn(io_H2S,is_FeS2)                = 1.0
+    conv_sed_ocn(io_H2S,is_FeS2)                = 7.0/8.0
+    conv_sed_ocn(io_SO4,is_FeS2)                = 1.0/8.0
     conv_sed_ocn(io_Fe,is_FeS2)                 = 1.0
-    conv_sed_ocn(io_H2S_34S,is_FeS2_34S)        = 1.0
+    conv_sed_ocn(io_ALK,is_FeS2)                = -2.0
+    conv_sed_ocn(io_H2S_34S,is_FeS2_34S)        = 7.0/8.0
+    conv_sed_ocn(io_SO4_34S,is_FeS2_34S)        = 1.0/8.0
     conv_sed_ocn(io_Fe_56Fe,is_FeS2_56Fe)       = 1.0
     ! convert dissolved species -> solid
     conv_ocn_sed(:,:) = 0.0
@@ -248,8 +251,11 @@ CONTAINS
     conv_ocn_sed(is_FeCO3_13C,io_DIC_13C)       = 1.0/conv_sed_ocn(io_DIC_13C,is_FeCO3_13C) 
     conv_ocn_sed(is_FeCO3_56Fe,io_Fe_56Fe)      = 1.0/conv_sed_ocn(io_Fe_56Fe,is_FeCO3_56Fe)
     conv_ocn_sed(is_FeS2,io_H2S)                = 1.0/conv_sed_ocn(io_H2S,is_FeS2)
+    conv_ocn_sed(is_FeS2,io_SO4)                = 1.0/conv_sed_ocn(io_SO4,is_FeS2)
     conv_ocn_sed(is_FeS2,io_Fe)                 = 1.0/conv_sed_ocn(io_Fe,is_FeS2)
+    conv_ocn_sed(is_FeS2,io_ALK)                = 1.0/conv_sed_ocn(io_ALK,is_FeS2)
     conv_ocn_sed(is_FeS2_34S,io_H2S_34S)        = 1.0/conv_sed_ocn(io_H2S_34S,is_FeS2_34S)
+    conv_ocn_sed(is_FeS2_34S,io_SO4_34S)        = 1.0/conv_sed_ocn(io_SO4_34S,is_FeS2_34S)
     conv_ocn_sed(is_FeS2_56Fe,io_Fe_56Fe)       = 1.0/conv_sed_ocn(io_Fe_56Fe,is_FeS2_56Fe) 
 
     ! ############################################################################################################################ !
