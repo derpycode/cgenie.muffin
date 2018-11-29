d2b699d81f1899b8bd02f362b8d5cd530ded358a
derpycode <derp@derpy.co.uk>
2018-11-28 17:51:15 -0800

    added Os tracer relationships

--- genie-main/src/fortran/cmngem/gem_util.f90
+++ genie-main/src/fortran/cmngem/gem_util.f90
@@ -176,6 +176,9 @@ CONTAINS
     conv_sed_ocn(io_Fe,is_FeOOH)                = 1.0
     conv_sed_ocn(io_Fe_56Fe,is_FeOOH_56Fe)      = 1.0
     conv_sed_ocn(io_O2,is_FeOOH)                = 1.0
+    conv_sed_ocn(io_Os,is_POM_Os)               = 1.0
+    conv_sed_ocn(io_Os_187Os,is_POM_Os_187Os)   = 1.0
+    conv_sed_ocn(io_Os_188Os,is_POM_Os_188Os)   = 1.0
     ! convert dissolved species -> solid
     conv_ocn_sed(:,:) = 0.0
     conv_ocn_sed(is_POC,io_DIC)              = 1.0/conv_sed_ocn(io_DIC,is_POC)
@@ -276,9 +279,12 @@ CONTAINS
     conv_ocn_sed(is_FeS2_34S,io_H2S_34S)        = 1.0/conv_sed_ocn(io_H2S_34S,is_FeS2_34S)
     conv_ocn_sed(is_FeS2_34S,io_SO4_34S)        = 1.0/conv_sed_ocn(io_SO4_34S,is_FeS2_34S)
     conv_ocn_sed(is_FeS2_56Fe,io_Fe2_56Fe)      = 1.0/conv_sed_ocn(io_Fe2_56Fe,is_FeS2_56Fe) 
-    conv_sed_ocn(is_FeOOH,io_Fe)                = 1.0/conv_sed_ocn(io_Fe,is_FeOOH)
-    conv_sed_ocn(is_FeOOH_56Fe,io_Fe_56Fe)      = 1.0/conv_sed_ocn(io_Fe_56Fe,is_FeOOH_56Fe)
-    conv_sed_ocn(is_FeOOH,io_O2)                = 1.0/conv_sed_ocn(io_O2,is_FeOOH)
+    conv_ocn_sed(is_FeOOH,io_Fe)                = 1.0/conv_sed_ocn(io_Fe,is_FeOOH)
+    conv_ocn_sed(is_FeOOH_56Fe,io_Fe_56Fe)      = 1.0/conv_sed_ocn(io_Fe_56Fe,is_FeOOH_56Fe)
+    conv_ocn_sed(is_FeOOH,io_O2)                = 1.0/conv_sed_ocn(io_O2,is_FeOOH)
+    conv_ocn_sed(is_POM_Os,io_Os)               = 1.0/conv_sed_ocn(io_Os,is_POM_Os)
+    conv_ocn_sed(is_POM_Os_187Os,io_Os_187Os)   = 1.0/conv_sed_ocn(io_Os_187Os,is_POM_Os_187Os)
+    conv_ocn_sed(is_POM_Os_188Os,io_Os_188Os)   = 1.0/conv_sed_ocn(io_Os_188Os,is_POM_Os_188Os)
     ! ############################################################################################################################ !
     ! DISSOLVED-PARTICULATE
     ! (compositional) relational operator for converting between DOM and POM
