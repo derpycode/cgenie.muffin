cdc9b6050a4ee49d043bce7a849636eedf6635f1
Markus Adloff <markus.adloff@bristol.ac.uk>
2020-03-18 17:17:50 +0000

    Corrected time stepping for Os scavenging

--- genie-biogem/src/fortran/biogem_box.f90
+++ genie-biogem/src/fortran/biogem_box.f90
@@ -4178,12 +4178,13 @@ CONTAINS
     loc_Os_188Os = dum_ocn_Os_188Os
     ! estimate Os scavenging
     ! NOTE: cap Os removal at the minimum of ([Os], [POC])
+    ! NOTE: select if scavenging should be dependent on ambient O2 concentration. If yes, Os is only scavenged below an adjustable O2 threshold
     loc_Os_scavenging = 0.0
     if ((ctrl_Os_scav_O2_dep) .and. (dum_ocn_O2 < par_scav_Os_O2_threshold)) then
-       loc_Os_scavenging = par_bio_remin_kOstoPOMOS*loc_Os*dum_dtyr*dum_bio_part(is2l(is_POC))
+       loc_Os_scavenging = par_bio_remin_kOstoPOMOS*loc_Os*dum_dt_scav*dum_bio_part(is2l(is_POC))
        loc_Os_scavenging = min(loc_Os_scavenging,dum_bio_part(is2l(is_POC)),loc_Os)
     elseif (.not. ctrl_Os_scav_O2_dep) then
-       loc_Os_scavenging = par_bio_remin_kOstoPOMOS*loc_Os*(dum_dt_scav/dum_dtyr)*dum_bio_part(is2l(is_POC))
+       loc_Os_scavenging = par_bio_remin_kOstoPOMOS*loc_Os*dum_dt_scav*dum_bio_part(is2l(is_POC))
        loc_Os_scavenging = min(loc_Os_scavenging,dum_bio_part(is2l(is_POC)),loc_Os)
     end if
     if (loc_Os_scavenging < const_real_nullsmall) loc_Os_scavenging = 0.0
