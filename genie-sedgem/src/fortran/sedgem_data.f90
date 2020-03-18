53b812a7fb71e13a642fb1e5136d2e735ae6f3a7
DomHu <dominik.huelse@ucr.edu>
2020-03-18 11:51:14 -0700

    Added options for Porg burial: 1) Porg = Corg 2) redox redendent P-regeneration. Added base- and user-config files to run example experiments. Added genie-paleo configuration with flat-bottom ocean.

--- genie-sedgem/src/fortran/sedgem_data.f90
+++ genie-sedgem/src/fortran/sedgem_data.f90
@@ -112,6 +112,8 @@ CONTAINS
        print*,'Include explicit P-cycle in OMEN-SED?               : ',par_sed_huelse2017_P_cycle
        print*,'Remove implicit Alk associated with buried sulf-OM? : ',par_sed_huelse2017_remove_impl_sulALK
        print*,'Simulate ocean Porg loss with buried sulf-OM?       : ',par_sed_huelse2017_sim_P_loss
+       print*,'Simulate ocean Porg loss related to Corg burial?    : ',par_sed_huelse2017_sim_P_loss_pres_fracC
+       print*,'Simulate increased P-regeneration under anoxia?     : ',par_sed_huelse2017_sim_P_regeneration
       ! --- DIAGENESIS SCHEME: ARCHER 1991 -------------------------------------------------------------------------------------- !
        print*,'--- DIAGENESIS SCHEME: ARCHER 1991 -----------------'
        print*,'dissolution rate constant, units of 1/s             : ',par_sed_archer1991_dissc
