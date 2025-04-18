682dbf034ffce61d96f035fa4d4fd554c93b11cb
derpycode <derp@derpy.co.uk>
2025-04-17 21:22:20 -0700

    fix for sedcore ncMAR

--- genie-sedgem/src/fortran/sedgem.f90
+++ genie-sedgem/src/fortran/sedgem.f90
@@ -234,7 +234,13 @@ SUBROUTINE sedgem(          &
                  dum_sfxsumsed(is_det,i,j) = dum_sfxsumsed(is_det,i,j) + &
                       & conv_m2_cm2*conv_det_g_mol*(conv_yr_kyr*loc_dtyr)*par_sed_fdet              
               endif
-           endif
+           endif           
+           ! if sedcore detrital (ncMAR) fluxes are specified -- completely replace det flux at those locations
+           ! NOTE: in the case of ctrl_sed_Fdet==.true, sedcore ncMAR has already replaced sed_Fsed_det at those locations
+           if (ctrl_sed_Fdet_sedcore .AND. sed_save_mask(i,j)) then
+              dum_sfxsumsed(is_det,i,j) = conv_m2_cm2*conv_det_g_mol*(conv_yr_kyr*loc_dtyr)*sed_Fsed_det(i,j)
+           end if
+           ! assign det age
            if (sed_select(is_det_age)) then
               if (ctrl_sed_Fdet) then
                  dum_sfxsumsed(is_det_age,i,j) = sed_age*conv_m2_cm2*conv_det_g_mol*(conv_yr_kyr*loc_dtyr)*sed_Fsed_det(i,j)
